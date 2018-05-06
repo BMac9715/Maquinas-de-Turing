a1fe37668faea76e509fffa476dab14ed0415a39
Bryan Macario <bryan.macario12@gmail.com>
2018-05-06 02:31:20 -0600

    Interfaz Grafica
    Beta

--- Simulador Maquinas de Turing/Simulador Maquinas de Turing/Form1.Designer.cs
+++ Simulador Maquinas de Turing/Simulador Maquinas de Turing/Form1.Designer.cs
@@ -28,12 +28,355 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblEntrada = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMaquinas = new System.Windows.Forms.Label();
+            this.rbtnSuma = new System.Windows.Forms.RadioButton();
+            this.rbtnResta = new System.Windows.Forms.RadioButton();
+            this.rbtnPalindromos = new System.Windows.Forms.RadioButton();
+            this.rbtnCopiar = new System.Windows.Forms.RadioButton();
+            this.rbtnMult = new System.Windows.Forms.RadioButton();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtInformacion = new System.Windows.Forms.TextBox();
+            this.lblInformacion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(772, 661);
+            this.splitContainer1.SplitterDistance = 316;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 155);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(748, 64);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(351, 85);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 20;
+            this.trackBar1.Location = new System.Drawing.Point(52, 155);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(247, 45);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(597, 20);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lblEntrada
+            // 
+            this.lblEntrada.AutoSize = true;
+            this.lblEntrada.Location = new System.Drawing.Point(14, 53);
+            this.lblEntrada.Name = "lblEntrada";
+            this.lblEntrada.Size = new System.Drawing.Size(44, 13);
+            this.lblEntrada.TabIndex = 6;
+            this.lblEntrada.Text = "Cadena";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Velocidad de Ejecución";
+            // 
+            // lblMaquinas
+            // 
+            this.lblMaquinas.AutoSize = true;
+            this.lblMaquinas.Location = new System.Drawing.Point(18, 21);
+            this.lblMaquinas.Name = "lblMaquinas";
+            this.lblMaquinas.Size = new System.Drawing.Size(158, 13);
+            this.lblMaquinas.TabIndex = 8;
+            this.lblMaquinas.Text = "Maquinas de Turing Disponibles";
+            // 
+            // rbtnSuma
+            // 
+            this.rbtnSuma.AutoSize = true;
+            this.rbtnSuma.Location = new System.Drawing.Point(42, 116);
+            this.rbtnSuma.Name = "rbtnSuma";
+            this.rbtnSuma.Size = new System.Drawing.Size(134, 17);
+            this.rbtnSuma.TabIndex = 9;
+            this.rbtnSuma.TabStop = true;
+            this.rbtnSuma.Text = "Suma en código unario";
+            this.rbtnSuma.UseVisualStyleBackColor = true;
+            // 
+            // rbtnResta
+            // 
+            this.rbtnResta.AutoSize = true;
+            this.rbtnResta.Location = new System.Drawing.Point(42, 139);
+            this.rbtnResta.Name = "rbtnResta";
+            this.rbtnResta.Size = new System.Drawing.Size(135, 17);
+            this.rbtnResta.TabIndex = 10;
+            this.rbtnResta.TabStop = true;
+            this.rbtnResta.Text = "Resta en código unario";
+            this.rbtnResta.UseVisualStyleBackColor = true;
+            // 
+            // rbtnPalindromos
+            // 
+            this.rbtnPalindromos.AutoSize = true;
+            this.rbtnPalindromos.Location = new System.Drawing.Point(41, 47);
+            this.rbtnPalindromos.Name = "rbtnPalindromos";
+            this.rbtnPalindromos.Size = new System.Drawing.Size(208, 17);
+            this.rbtnPalindromos.TabIndex = 11;
+            this.rbtnPalindromos.TabStop = true;
+            this.rbtnPalindromos.Text = "Reconocedor de cadenas palindromas";
+            this.rbtnPalindromos.UseVisualStyleBackColor = true;
+            // 
+            // rbtnCopiar
+            // 
+            this.rbtnCopiar.AutoSize = true;
+            this.rbtnCopiar.Location = new System.Drawing.Point(41, 70);
+            this.rbtnCopiar.Name = "rbtnCopiar";
+            this.rbtnCopiar.Size = new System.Drawing.Size(111, 17);
+            this.rbtnCopiar.TabIndex = 12;
+            this.rbtnCopiar.TabStop = true;
+            this.rbtnCopiar.Text = "Copia de patrones";
+            this.rbtnCopiar.UseVisualStyleBackColor = true;
+            // 
+            // rbtnMult
+            // 
+            this.rbtnMult.AutoSize = true;
+            this.rbtnMult.Location = new System.Drawing.Point(41, 93);
+            this.rbtnMult.Name = "rbtnMult";
+            this.rbtnMult.Size = new System.Drawing.Size(171, 17);
+            this.rbtnMult.TabIndex = 13;
+            this.rbtnMult.TabStop = true;
+            this.rbtnMult.Text = "Multiplicación en código unario";
+            this.rbtnMult.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Location = new System.Drawing.Point(12, 3);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.trackBar1);
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            this.splitContainer2.Panel1.Controls.Add(this.button3);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer2.Panel1.Controls.Add(this.lblEntrada);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.lblInformacion);
+            this.splitContainer2.Panel2.Controls.Add(this.txtInformacion);
+            this.splitContainer2.Panel2.Controls.Add(this.rbtnPalindromos);
+            this.splitContainer2.Panel2.Controls.Add(this.rbtnMult);
+            this.splitContainer2.Panel2.Controls.Add(this.lblMaquinas);
+            this.splitContainer2.Panel2.Controls.Add(this.rbtnCopiar);
+            this.splitContainer2.Panel2.Controls.Add(this.rbtnSuma);
+            this.splitContainer2.Panel2.Controls.Add(this.rbtnResta);
+            this.splitContainer2.Size = new System.Drawing.Size(748, 326);
+            this.splitContainer2.SplitterDistance = 348;
+            this.splitContainer2.TabIndex = 14;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(679, 20);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.lblTitulo);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.pictureBox3);
+            this.splitContainer3.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer3.Panel2.Controls.Add(this.pictureBox4);
+            this.splitContainer3.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer3.Size = new System.Drawing.Size(766, 305);
+            this.splitContainer3.SplitterDistance = 61;
+            this.splitContainer3.TabIndex = 4;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(127, 16);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(483, 33);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Simulador de Maquinas de Turing";
+            // 
+            // txtInformacion
+            // 
+            this.txtInformacion.Location = new System.Drawing.Point(21, 195);
+            this.txtInformacion.Multiline = true;
+            this.txtInformacion.Name = "txtInformacion";
+            this.txtInformacion.Size = new System.Drawing.Size(361, 112);
+            this.txtInformacion.TabIndex = 14;
+            // 
+            // lblInformacion
+            // 
+            this.lblInformacion.AutoSize = true;
+            this.lblInformacion.Location = new System.Drawing.Point(18, 177);
+            this.lblInformacion.Name = "lblInformacion";
+            this.lblInformacion.Size = new System.Drawing.Size(167, 13);
+            this.lblInformacion.TabIndex = 15;
+            this.lblInformacion.Text = "Informacion acerca de la máquina";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 661);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblEntrada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtnPalindromos;
+        private System.Windows.Forms.RadioButton rbtnMult;
+        private System.Windows.Forms.Label lblMaquinas;
+        private System.Windows.Forms.RadioButton rbtnCopiar;
+        private System.Windows.Forms.RadioButton rbtnSuma;
+        private System.Windows.Forms.RadioButton rbtnResta;
+        private System.Windows.Forms.TextBox txtInformacion;
+        private System.Windows.Forms.Label lblInformacion;
     }
 }
 
